80e49dc23703768a20958fab63628d832714dcfc
baselarafat <basil.arafat@richmond.edu>
2025-02-22 23:18:07 -0600

    add Project and Navigation components with styling and responsive design

---
+++ src/components/Project.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../assets/styles/Project.scss';
+
+function Project() {
+    return(
+    <div className="projects-container" id="projects">
+        <h1>Personal Projects</h1>
+      
+    </div>
+    );
+}
+
+export default Project;
